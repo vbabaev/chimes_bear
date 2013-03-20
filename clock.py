b9470f843693a7a3739de215945efdc152ec79bc
vbabaev <gglbabaev@gmail.com>
2013-03-20 22:10:02 +0000

    clock's class

---
+++ clock.py
@@ -0,0 +1,14 @@
+import time
+import datetime
+
+class Clock:
+	"""Simple talkink clock"""
+		
+	def __init__(self):
+		self.date = datetime.datetime.fromtimestamp(time.time())
+
+	def do_hourly_bong(self):
+		print self.date.hour
+	def do_definite_bong(self):
+		print self.date.strftime("%H_%M")
+
