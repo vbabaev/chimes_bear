cdaccd7aa28cd794c6fbe6f17044ec5cab079337
Vadim Babaev <gglbabaev@gmail.com>
2013-03-24 01:47:26 +0400

    доработал загрузку конфига

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 import json
+import os
 
 class Config:
 	def __init__(self):
-		data = open('config.json', 'r')
+		directory = os.path.dirname(os.path.realpath(__file__))
+		data = open(directory + '/config.json', 'r')
 		self.data = json.load(data)
 	def get(self, key):
 		return self.data[key]
